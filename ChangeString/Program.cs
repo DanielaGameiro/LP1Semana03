5f97f0cebac9738cdf929bf8a53c6535afa50ce1
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-15 13:05:03 +0000

    Solve ChangeString exercise

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            char inputChar;
+
+            Console.WriteLine("Write a string: ");
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Write a char: ");
+            inputChar = char.Parse(Console.ReadLine());
+
+            foreach (char c in inputString)
+            {
+                if (c != inputChar)
+                {
+                    Console.Write(c);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
